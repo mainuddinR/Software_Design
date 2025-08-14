9643b05eb0b1cae4ffcb619c0d7b64efa03bb668
mainuddinR <bsse1304@iit.du.ac.bd>
2025-08-14 23:57:17 +0600

    r

--- UserRoleManagement/src/main/java/com/example/usermanagement/application/interfaces/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.usermanagement.application.interfaces;
-
-import com.example.usermanagement.domain.*;
-import java.util.*;
-
-public interface RoleRepository {
-    UUID save(Role role);
-    Optional<Role> findById(UUID id);
-}
--- UserRoleManagement/src/main/java/com/example/usermanagement/infrastructure/persistence/RoleJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.usermanagement.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-
-public interface RoleJpaRepository extends JpaRepository<RoleJpaEntity, UUID> {}
